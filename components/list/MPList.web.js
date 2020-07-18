a1f88326ac6bf32b5ce13ccf320419c5124df979
Dusan Miloradovic <dusan.m@maximoplus.com>
2020-07-18 18:06:20 +0400

    mplist web

--- components/list/MPList.web.js
+++ components/list/MPList.web.js
@@ -1,27 +1,28 @@
 import React, { useState, PureComponent } from "react";
 import MPListItem from "./MPListItem";
 import { FlatList, View, ActivityIndicator } from "react-native";
+import { useInView } from "react-intersection-observer";
 
 export default props => {
-  const [fetching, setFetching] = useState(null); //to prevent from excessive fetching
+  const [fetching, setFetching] = useState(false); //to prevent from excessive fetching
 
-  const FooterWait = () => {
-    return props.waiting ? (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          paddingVertical: 10
-        }}
-      >
-        <ActivityIndicator animating size="small" />
-      </View>
-    ) : null;
+  const FooterLoader = () => {
+    const [ref, inView, entry] = useInView();
+
+    if (inView && !fetching) {
+      setFetching(true);
+      props.fetchMore(5);
+      setTimeout(() => setFetching(false), 200);
+    }
+    return (
+      <div ref={ref} style={{ width: "100%", height: "50px" }}>
+        {fetching ? <ActivityIndicator animating size="large" /> : null}
+      </div>
+    );
   };
+
   if (!props.data || props.data.length === 0) return null;
-  console.log(props.data);
+  console.log(props);
   return (
     <div style={{ height: "100%", width: "100%", overflowY: "scroll" }}>
       <ul
@@ -39,6 +40,7 @@ export default props => {
           />
         ))}
       </ul>
+      <FooterLoader />
     </div>
   );
 };

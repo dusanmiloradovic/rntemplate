a331c0c7eaa85396f3f075ca2214a0b8c670c427
Dusan Miloradovic <dusan.m@maximoplus.com>
2021-03-30 19:40:18 +0400

    as i suspected things have to be fixed because of expo version
    c

--- src/utils/boot.js
+++ src/utils/boot.js
@@ -2,8 +2,8 @@ import "maximoplus-core-native";
 import { SERVER_ROOT } from "@env";
 import { setServerRoot } from "mplus-react";
 
-console.log("maximoplus core native module loaded");
-console.log("server root=" + SERVER_ROOT);
+console.log("##maximoplus core native module loaded");
+console.log("--server root=" + SERVER_ROOT);
 setServerRoot(SERVER_ROOT);
 
 export const getServerRoot = () => SERVER_ROOT;
